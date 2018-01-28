f35857f886e9e560163af9c0590b9069c253ac27
kimkundad <kim.kundad@gmail.com>
2018-01-28 17:19:38 +0700

    first commit

--- resources/views/refer.blade.php
+++ resources/views/refer.blade.php
@@ -212,7 +212,7 @@ ul.list_order li span {
                             <div class="col-md-12">
                             <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
 
-                              <div class="g-recaptcha" data-sitekey="6LeJ1yUUAAAAACtPVyLQ5AhlsaiXX-Lv_BvAyoHM"></div>
+                              <div class="g-recaptcha" data-sitekey="6LdHOiUUAAAAAAobn_eaNZe6ieq_s1aE9fXK26_y"></div>
                               @if ($errors->has('g-recaptcha-response'))
                                   <span class="help-block">
                                       <strong>Robot?!</strong>
