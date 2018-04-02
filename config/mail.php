39ea766e49d97f9a02d0c54517ccfac7cd89fae6
kimkundad <kim.kundad@gmail.com>
2018-04-02 21:44:24 +0700

    golfd

--- config/mail.php
+++ config/mail.php
@@ -55,10 +55,8 @@ return [
     |
     */
 
-    'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+
+    'from' => ['address' => 'infosirispace@gmail.com', 'name' => 'sirispace'],
 
     /*
     |--------------------------------------------------------------------------
