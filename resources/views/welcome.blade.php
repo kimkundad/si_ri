5466288aff7dd90f53688760d54803e3d0c23a7f
kimkundad <kim.kundad@gmail.com>
2018-07-08 06:53:17 +0700

    golfss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1043,7 +1043,7 @@ button.onclick = function() {
     }
 };
 </script>
-
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
 
 <script type="text/javascript">
