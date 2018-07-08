6de7820fba3f0a4316fbc2336846b600cb420744
kimkundad <kim.kundad@gmail.com>
2018-07-08 07:15:36 +0700

    golfss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1053,16 +1053,30 @@ button.onclick = function() {
         $('.typeahead').typeahead({
 
             source:  function (query, process) {
+
             return $.get(url, { query: query }, function (data) {
-        
-                    return data.value;
+
+                    return process(data);
 
                 });
 
             }
 
+        });
 
-
+        $input.change(function() {
+          var current = $input.typeahead("getActive");
+          if (current) {
+            // Some item from your model is active!
+            if (current.name == $input.val()) {
+              // This means the exact match is found. Use toLowerCase() if you want case insensitive match.
+            } else {
+              // This means it is only a partial match, you can either add a new item
+              // or take the active if you don't want new items
+            }
+          } else {
+            // Nothing is active so it is a new value (or maybe empty value)
+          }
         });
 
     </script>
